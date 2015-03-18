ad71f3538dbd515246eaa0f0b067bffbeea6621f
bugalood <jerome@taotesting.com>
2015-03-18 16:14:45 +0100

    A little bit more docs.

--- model/PlatformThemingService.php
+++ model/PlatformThemingService.php
@@ -120,8 +120,11 @@ class PlatformThemingService extends tao_models_classes_Service
     /**
      * Get a reference on the data storage directory.
      * 
+     * You can call core_kernel_file_File::getAbsolutePath() and/or core_kernel_file_File::getRelativePath()
+     * on the return object to know where to store data assets.
+     * 
      * @return core_kernel_file_File
-     * @throws common_exception If no default repositr
+     * @throws common_exception If no default data storage directory is configured.
      */
     public function getDataDirectory()
     {
